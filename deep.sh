a4bebea9761e694b9525288ab01b786438d9d6c3
Kun Qian <kun>
2022-05-31 16:36:48 -0400

    deeps.sh

--- deep.sh
+++ deep.sh
@@ -1,4 +1,13 @@
-deepspeed main.py --dataset mwoz --task fine_tune --n-epochs 7 --do-train \
-      --style dataset --model gpt --size medium --num-shots full --batch-size 4 \
-      --learning-rate 1e-5  --maximum-len 512 --prompt-style naive --seed 27 \
-      --verbose --prune-keep 2 --fp16 --fp16_backend 'amp' --deepspeed ds_config.json
+#!/bin/bash
+#
+set -xue
+# deepspeed main.py --dataset mwoz --task fine_tune --n-epochs 7 --do-train \
+#       --style dataset --model gpt --size medium --num-shots full --batch-size 4 \
+#       --learning-rate 1e-5  --maximum-len 512 --prompt-style naive --seed 27 \
+#       --verbose --prune-keep 2 --fp16 --fp16_backend 'amp' --deepspeed ds_config.json
+
+python main.py \
+      --dataset mwoz --task fine_tune --n-epochs 10 --do-train \
+      --style dataset --model t5 --size large --num-shots full --batch-size 16 --grad-accum-steps 16\
+      --learning-rate 3e-2  --maximum-len 512 --prompt-style naive \
+      --verbose --prune-keep 2 --fp16 --ignore-cache --parallel
