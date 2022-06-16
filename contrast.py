34d933af2085d42bf1bbead33a154edafb219a09
Derek Chen <dchen@asapp.com>
2022-06-15 20:23:58 -0400

    Evaluator for custom contrastive loss

--- contrast.py
+++ contrast.py
@@ -54,85 +54,6 @@ class DomainSlotValueLoss(nn.Module):
     # labels = labels.contiguous().view(-1, 1)
     return converted
 
-  def sup_con_loss(self, sentence_features, labels):
-    """ supervised contrastive loss """
-    embeds = [self.model(sent_feat)['sentence_embedding'] for sent_feat in sentence_features]
-    # embeds[0] has shape batch_size, embed_dim
-    assert len(embeds) == 2
-
-    # transform labels vector into a matrix of 0.0s and 1.0s (aka. floats)
-    mask = torch.eq(labels, labels.T).float().to(device)
-    
-    # Reshapes the embeddings to merge the batch_size and number of positive classes
-    anchor_count = embeds.shape[1]   # should be 3 for Domain, Slot and Value
-    anchor_feature = torch.cat(torch.unbind(embeds, dim=1), dim=0)  # only embed_dim remains
-
-    anchor_dot_contrast = torch.div(
-      torch.matmul(anchor_feature, contrast_feature.T),
-      self.temperature)
-    # for numerical stability
-    logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-    logits = anchor_dot_contrast - logits_max.detach()
-
-    # tile mask
-    mask = mask.repeat(anchor_count, contrast_count)
-    # mask-out self-contrast cases
-    logits_mask = torch.scatter(
-      torch.ones_like(mask),
-      1,
-      torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
-      0
-    )
-    mask = mask * logits_mask
-
-    # compute log_prob
-    exp_logits = torch.exp(logits) * logits_mask
-    log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
-
-    # compute mean of log-likelihood over positive
-    mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-
-    # loss
-    loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
-    loss = loss.view(anchor_count, batch_size).mean()
-
-    return loss
-
-  def simclr_loss(self, sentence_features, labels):
-    """ modified SimCLR loss """
-    embeds = [self.model(sent_feat)['sentence_embedding'] for sent_feat in sentence_features]
-    z_i = F.normalize(embeds[0], dim=1)
-    z_j = F.normalize(embeds[1], dim=1)
-    rep = torch.cat([z_i, z_j], dim=0)
-
-    similarity_matrix = F.cosine_similarity(rep.unsqueeze(1), rep.unsqueeze(0), dim=2)
-    if self.verbose: print("Similarity matrix", similarity_matrix.shape)
-  
-    def pairwise_loss(i, j):
-      z_i_, z_j_ = rep[i], rep[j]
-      sim_i_j = similarity_matrix[i, j]
-      if self.verbose: print(f"sim({i}, {j})={sim_i_j}")
-          
-      numerator = torch.exp(sim_i_j / self.temperature)
-      default_ones = torch.ones((2 * self.batch_size, ))
-      one_for_not_i = default_ones.to(emb_i.device).scatter_(0, torch.tensor([i]), 0.0)
-      if self.verbose: print(f"one for not i", one_for_not_i.shape)
-      
-      partition_func = torch.exp(similarity_matrix[i, :] / self.temperature)
-      denominator = torch.sum(one_for_not_i * partition_func)
-      if self.verbose: print("Denominator", denominator.shape)
-          
-      loss_ij = -torch.log(numerator / denominator)
-      if self.verbose: print(f"loss({i},{j})={loss_ij}")
-      return loss_ij.squeeze(0)
-
-    N = self.batch_size
-    total_loss = 0.0
-    for k in range(0, N):
-        total_loss += self.pairwise_loss(k, k + N) + self.pairwise_loss(k + N, k)
-    total_loss *= 1.0 / (2*N)
-    return total_loss
-
   def forward(self, sentence_features, targets):
     # sentence_features is Iterable[Dict[str, Tensor]]
     # labels is an integer representing a binary encoding
@@ -183,11 +104,15 @@ def fit_model(args, model, dataloader, evaluator):
   return model
 
 def build_evaluator(args, dev_samples):
-  print("Building the evaluator which is cosine similarity by default")
-  evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, 
-      show_progress_bar=False, name='mwoz', write_csv=args.do_save)
-  # InformationRetrievalEvaluator, RerankingEvaluator
   # https://www.sbert.net/docs/package_reference/evaluation.html
+  print("Building the evaluator which is cosine similarity by default")
+  if args.loss_function == 'custom':
+    queries, corpus, relevant_docs = dev_samples
+    evaluator = InformationRetrievalEvaluator(queries, corpus, relevant_docs,
+          precision_recall_at_k=[1, 5, 10], show_progress_bar=True, name="margin_4|7|10")
+  else:
+    evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, 
+          show_progress_bar=False, name='mwoz', write_csv=args.do_save)
   # alternatively, where sentences1, sentences2, and scores are lists of equal length
   # evaluator = EmbeddingSimilarityEvaluator(sentences1, sentences2, scores)
   return evaluator
@@ -218,6 +143,17 @@ def mine_for_samples(args):
   print('train size', len(selected_pairs), 'dev size', len(dev_pairs))
   return selected_pairs, dev_pairs
 
+def mine_for_queries(args):
+  # Load raw dialogue data from cache
+  samples = load_from_cache(args)
+  # Look for positive and negative example pairs to be used as training data based on similarity
+  all_pairs = compute_scores(args, samples)
+  divide = int(len(all_pairs) * 0.8)
+  selected_pairs = all_pairs[:divide]
+  # Gather the queries, corpus and relevant docs
+  dev_data = gather_docs(args, samples[divide:])
+  return selected_pairs, dev_data
+
 def select_test_data(args):
   samples = load_from_cache(args)
   total_size = args.kappa * args.batch_size
@@ -235,6 +171,37 @@ def find_positives_negatives(samples):
       pair = {'si': s_i['history'], 'sj': s_j['history'], 'score': sim_score}
       all_pairs.append(pair)
   return all_pairs
+
+def gather_docs(args, samples):
+  """ queries is a dict with key of qid and value of utterance string
+  corpus is a dict with key of cid and value of utterance string
+  relevant docs is a dict with key of qid and value is a list of cids that are relevant
+  """
+  corpus, queries = {}, {}
+  relevant_docs = defaultdict(list)
+
+  for out_sample in progress_bar(samples, total=len(samples), desc='Gathering queries'):
+    cid = out_sample['gid']
+    dialogue = out_sample['history']
+    corpus[cid] = dialogue
+
+    for in_sample in samples:
+      qid = in_sample['gid']
+      if cid == qid: continue
+
+      domain, slot, value = in_sample['dsv']
+      valid = slot in ['internet', 'parking']  # default to False most of the time
+      for part in value.lower().replace(':', ' ').replace('|', ' ').split():
+        if part in s_j['history'].lower():
+          valid = True
+      if not valid: continue
+
+      sim_score = domain_slot_sim(in_sample, out_sample)
+      if sim_score > 0.6:
+        relevant_docs[qid].append(cid)
+        queries[qid] = in_sample['history']
+      
+  return queries, corpus, relevant_docs
 
 def compute_scores(args, samples):
   """ InputExample is an object that requires the following API params: 
@@ -362,7 +329,10 @@ if __name__ == "__main__":
   model.to(device)
 
   if args.do_train:
-    train_samples, dev_samples = mine_for_samples(args)
+    if args.loss_function == 'custom':
+      train_samples, dev_samples = mine_for_queries(args)
+    else:
+      train_samples, dev_samples = mine_for_samples(args)
     dataloader = DataLoader(train_samples, shuffle=True, batch_size=args.batch_size)
     evaluator = build_evaluator(args, dev_samples)
     fit_model(args, model, dataloader, evaluator)
@@ -374,44 +344,80 @@ if __name__ == "__main__":
     test_model(args, model, dataloader)
 
 """
-class ContrastiveLossELI5(nn.Module):
-  def __init__(self, batch_size, temperature=0.5, verbose=True):
-    super().__init__()
-    self.batch_size = batch_size
-    self.register_buffer("temperature", torch.tensor(temperature))
-    self.verbose = verbose
-          
-  def forward(self, emb_i, emb_j):
-    z_i = F.normalize(emb_i, dim=1)
-    z_j = F.normalize(emb_j, dim=1)
+  def sup_con_loss(self, sentence_features, labels):
+    embeds = [self.model(sent_feat)['sentence_embedding'] for sent_feat in sentence_features]
+    # embeds[0] has shape batch_size, embed_dim
+    assert len(embeds) == 2
 
+    # transform labels vector into a matrix of 0.0s and 1.0s (aka. floats)
+    mask = torch.eq(labels, labels.T).float().to(device)
+    
+    # Reshapes the embeddings to merge the batch_size and number of positive classes
+    anchor_count = embeds.shape[1]   # should be 3 for Domain, Slot and Value
+    anchor_feature = torch.cat(torch.unbind(embeds, dim=1), dim=0)  # only embed_dim remains
+
+    anchor_dot_contrast = torch.div(
+      torch.matmul(anchor_feature, contrast_feature.T),
+      self.temperature)
+    # for numerical stability
+    logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+    logits = anchor_dot_contrast - logits_max.detach()
+
+    # tile mask
+    mask = mask.repeat(anchor_count, contrast_count)
+    # mask-out self-contrast cases
+    logits_mask = torch.scatter(
+      torch.ones_like(mask),
+      1,
+      torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
+      0
+    )
+    mask = mask * logits_mask
+
+    # compute log_prob
+    exp_logits = torch.exp(logits) * logits_mask
+    log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+    # compute mean of log-likelihood over positive
+    mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+    # loss
+    loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+    loss = loss.view(anchor_count, batch_size).mean()
+
+    return loss
+
+  def simclr_loss(self, sentence_features, labels):
+    embeds = [self.model(sent_feat)['sentence_embedding'] for sent_feat in sentence_features]
+    z_i = F.normalize(embeds[0], dim=1)
+    z_j = F.normalize(embeds[1], dim=1)
     rep = torch.cat([z_i, z_j], dim=0)
+
     similarity_matrix = F.cosine_similarity(rep.unsqueeze(1), rep.unsqueeze(0), dim=2)
-    if self.verbose: print("Similarity matrix\n", similarity_matrix, "\n")
-        
-    def l_ij(i, j):
-        z_i_, z_j_ = rep[i], rep[j]
-        sim_i_j = similarity_matrix[i, j]
-        if self.verbose: print(f"sim({i}, {j})={sim_i_j}")
-            
-        numerator = torch.exp(sim_i_j / self.temperature)
-        default_ones = torch.ones((2 * self.batch_size, ))
-        one_for_not_i = default_ones.to(emb_i.device).scatter_(0, torch.tensor([i]), 0.0)
-        if self.verbose: print(f"1{{k!={i}}}",one_for_not_i)
-        
-        denominator = torch.sum(
-            one_for_not_i * torch.exp(similarity_matrix[i, :] / self.temperature)
-        )    
-        if self.verbose: print("Denominator", denominator)
-            
-        loss_ij = -torch.log(numerator / denominator)
-        if self.verbose: print(f"loss({i},{j})={loss_ij}\n")
-            
-        return loss_ij.squeeze(0)
+    if self.verbose: print("Similarity matrix", similarity_matrix.shape)
+  
+    def pairwise_loss(i, j):
+      z_i_, z_j_ = rep[i], rep[j]
+      sim_i_j = similarity_matrix[i, j]
+      if self.verbose: print(f"sim({i}, {j})={sim_i_j}")
+          
+      numerator = torch.exp(sim_i_j / self.temperature)
+      default_ones = torch.ones((2 * self.batch_size, ))
+      one_for_not_i = default_ones.to(emb_i.device).scatter_(0, torch.tensor([i]), 0.0)
+      if self.verbose: print(f"one for not i", one_for_not_i.shape)
+      
+      partition_func = torch.exp(similarity_matrix[i, :] / self.temperature)
+      denominator = torch.sum(one_for_not_i * partition_func)
+      if self.verbose: print("Denominator", denominator.shape)
+          
+      loss_ij = -torch.log(numerator / denominator)
+      if self.verbose: print(f"loss({i},{j})={loss_ij}")
+      return loss_ij.squeeze(0)
 
     N = self.batch_size
-    loss = 0.0
+    total_loss = 0.0
     for k in range(0, N):
-        loss += l_ij(k, k + N) + l_ij(k + N, k)
-    return 1.0 / (2*N) * loss
+        total_loss += self.pairwise_loss(k, k + N) + self.pairwise_loss(k + N, k)
+    total_loss *= 1.0 / (2*N)
+    return total_loss
 """
